8490dbeee4eb0ff9a481e995835d538a90044944
Katsuyoshi Ito <kito@itosoft.com>
2023-12-27 10:16:22 +0900

    add tests
    - add tests to send simple text message
    - rename
        send_message_to_talkroom to
        send_messages_to_talkroom
    - add send_messages_to_user

--- lib/line_works/bot/client.rb
+++ lib/line_works/bot/client.rb
@@ -1,5 +1,7 @@
 require 'line/bot'
 
+# @see: message types https://developers.worksmobile.com/jp/docs/bot-send-content
+
 module LineWorks
   module Bot
     # API Client of Line Works Bot SDK Ruby
@@ -15,28 +17,59 @@ module LineWorks
         @endpoint ||= API::DEFAULT_ENDPOINT
       end
 
-      # Send messages to a channel using channel_id.
+      # Send messages to a talkroom (channel) using channel_id.
+      # @see: https://developers.worksmobile.com/jp/docs/bot-channel-message-send
       #
       # @param bot_id [String] Bot Id
       # @param channel_id [String] Channel Id
-      # @param messages [String] Message Objects
+      # @param messages [String, Hash] Message Objects
       # @param headers [Hash] HTTP Headers
       # @param payload [Hash] Additional request body
       # @return [Net::HTTPResponse]
-      def send_message_to_talkroom(bot_id, channel_id, messages, headers: {})
+      def send_messages_to_talkroom(bot_id, channel_id, messages, headers: {}, payload: {})
         channel_token_required
 
-        payload = {
-          "content" => {
-            "type" => "text",
-            "text" => messages
+        case messages
+        when String
+          payload[:content] = {
+            type: "text",
+            text: messages
           }
-        }
+        when Hash
+          payload[:content] = messages
+        end
     
         endpoint_path = "/bots/#{bot_id}/channels/#{channel_id}/messages"
 
         payload = payload.to_json
-p [endpoint_path, payload]
+        post(endpoint, endpoint_path, payload, credentials.merge(headers))
+      end
+
+      # Send messages to a user using user_id.
+      # @see: https://developers.worksmobile.com/jp/docs/bot-user-message-send
+      #
+      # @param bot_id [String] Bot Id
+      # @param user_id [String] User Id
+      # @param messages [String, Hash] Message Objects
+      # @param headers [Hash] HTTP Headers
+      # @param payload [Hash] Additional request body
+      # @return [Net::HTTPResponse]
+      def send_messages_to_user(bot_id, user_id, messages, headers: {}, payload: {})
+        channel_token_required
+
+        case messages
+        when String
+          payload[:content] = {
+            type: "text",
+            text: messages
+          }
+        when Hash
+          payload[:content] = messages
+        end
+    
+        endpoint_path = "/bots/#{bot_id}/users/#{user_id}/messages"
+
+        payload = payload.to_json
         post(endpoint, endpoint_path, payload, credentials.merge(headers))
       end
 
---
+++ spec/line_works/send_message_01_text_spec.rb
@@ -0,0 +1,95 @@
+require 'spec_helper'
+require 'webmock/rspec'
+require 'json'
+
+describe LineWorks::Bot::Client do
+
+  it 'send the text message to the talkroom' do
+    uri_template = Addressable::Template.new LineWorks::Bot::API::DEFAULT_ENDPOINT + '/bots/bot_id/channels/channel_id/messages'
+    stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
+
+    client = LineWorks::Bot::Client.new do |config|
+      config.channel_token = 'channel_token'
+    end
+
+    bot_id = 'bot_id'
+    channel_id = 'channel_id'
+    message = 'Hello, world'
+    response = client.send_messages_to_talkroom(bot_id, channel_id, message)
+
+    expected = {
+      content: {
+        type: 'text',
+        text: message
+      }
+    }.to_json
+    expect(response.body).to eq(expected)
+  end
+
+  it 'send the text message in a hash to the talkroome' do
+    uri_template = Addressable::Template.new LineWorks::Bot::API::DEFAULT_ENDPOINT + '/bots/bot_id/channels/channel_id/messages'
+    stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
+
+    client = LineWorks::Bot::Client.new do |config|
+      config.channel_token = 'channel_token'
+    end
+
+    bot_id = 'bot_id'
+    channel_id = 'channel_id'
+    message = {
+      type: 'text',
+      text: 'Hello, world'
+    }
+    response = client.send_messages_to_talkroom(bot_id, channel_id, message)
+
+    expected = {
+      content: message
+    }.to_json
+    expect(response.body).to eq(expected)
+  end
+
+  it 'send the text message to the user' do
+    uri_template = Addressable::Template.new LineWorks::Bot::API::DEFAULT_ENDPOINT + '/bots/bot_id/users/user_id/messages'
+    stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
+
+    client = LineWorks::Bot::Client.new do |config|
+      config.channel_token = 'channel_token'
+    end
+
+    bot_id = 'bot_id'
+    user_id = 'user_id'
+    message = 'Hello, world'
+    response = client.send_messages_to_user(bot_id, user_id, message)
+
+    expected = {
+      content: {
+        type: 'text',
+        text: message
+      }
+    }.to_json
+    expect(response.body).to eq(expected)
+  end
+
+  it 'send the text message in a hash to the user' do
+    uri_template = Addressable::Template.new LineWorks::Bot::API::DEFAULT_ENDPOINT + '/bots/bot_id/users/user_id/messages'
+    stub_request(:post, uri_template).to_return { |request| {body: request.body, status: 200} }
+
+    client = LineWorks::Bot::Client.new do |config|
+      config.channel_token = 'channel_token'
+    end
+
+    bot_id = 'bot_id'
+    user_id = 'user_id'
+    message = {
+      type: 'text',
+      text: 'Hello, world'
+    }
+    response = client.send_messages_to_user(bot_id, user_id, message)
+
+    expected = {
+      content: message
+    }.to_json
+    expect(response.body).to eq(expected)
+  end
+
+end
